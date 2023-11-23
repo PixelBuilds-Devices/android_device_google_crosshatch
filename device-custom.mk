71a081896eb1217c7363fce524b23679fc432735
DhineshCool <dhineshcool585@gmail.com>
2023-11-23 19:22:24 +0530

    Revert "b1c1: Add shim for missing symbol in libsecureuisvc_jni.so"
    This reverts commit e053d44033f289de4f42b90d3ed07143a7df778c.

--- device-custom.mk
+++ device-custom.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.frameworks.sensorservice@1.0.vendor
 
-# Shims
-PRODUCT_PACKAGES += \
-    lib-secureuishim
-
 # Utilities
 PRODUCT_PACKAGES += \
     libjson \
