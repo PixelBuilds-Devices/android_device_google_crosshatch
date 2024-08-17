6f3febc1d1a63135de2a5e3708cc37cd4be90f19
DhineshCool <dhineshcool585@gmail.com>
2024-08-17 13:40:22 +0530

    b1c1: drop stock fingerprint

--- aosp_blueline.mk
+++ aosp_blueline.mk
@@ -73,9 +73,6 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=blueline \
-    PRIVATE_BUILD_DESC="blueline-user 12 SP1A.210812.016.C2 8618562 release-keys"
-
-BUILD_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.016.C2/8618562:user/release-keys
+    PRODUCT_NAME=blueline
 
 $(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
